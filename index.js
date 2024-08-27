2af595c8e0551089011611d99a858453cd1ccae5
LowellTebrero <lowelltebrero@gmail.com>
2024-08-27 22:40:54 +0800

    new update

--- index.js
+++ index.js
@@ -71,3 +71,39 @@ if (previouslyActiveTab && previouslyActiveButtonIndex) {
   // Default to opening the first tab if no active tab is stored
   openTab("graphics", document.getElementsByClassName("tablink")[0]);
 }
+
+
+
+// Get the main featured image element
+const featuredImage = document.getElementById('featured-image').querySelector('img');
+
+// Get all the thumbnail images and their wrappers
+const thumbnailWrappers = document.querySelectorAll('.flex-none');
+
+// Store the currently featured thumbnail wrapper
+let currentThumbnailWrapper = null;
+
+// Add click event listener to each thumbnail wrapper
+thumbnailWrappers.forEach(wrapper => {
+    const thumbnail = wrapper.querySelector('.thumbnail');
+    thumbnail.addEventListener('click', function() {
+        // If the clicked thumbnail is the currently featured one, unhide it and reset the main image
+        if (currentThumbnailWrapper === wrapper) {
+            wrapper.style.display = "inline-block"; // Show the entire wrapper
+            currentThumbnailWrapper = null; // Reset the current thumbnail wrapper
+            featuredImage.src = ""; // Reset the featured image (optional: you can set a default image)
+        } else {
+            // Show the previously hidden wrapper (if any)
+            if (currentThumbnailWrapper) {
+                currentThumbnailWrapper.style.display = "inline-block"; // Show the previous wrapper
+            }
+
+            // Update the main featured image
+            featuredImage.src = thumbnail.src;
+
+            // Hide the clicked wrapper
+            wrapper.style.display = "none";
+            currentThumbnailWrapper = wrapper; // Set this wrapper as the currently featured one
+        }
+    });
+});
